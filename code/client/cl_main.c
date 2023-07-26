24863d3d1bc95948b9290713efd2543859ee116e
bh247484 <bh247484@gmail.com>
2023-07-26 18:24:03 -0400

    Revert unused test changes to quake engine

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -23,12 +23,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "client.h"
 #include <limits.h>
-#include <stdio.h>
 
 #include "../sys/sys_local.h"
 #include "../sys/sys_loadlib.h"
-
-#include <WiseWrapper.h>
 
 #ifdef USE_MUMBLE
 #include "libmumblelink.h"
@@ -3166,8 +3163,6 @@ void CL_StartHunkUsers( qboolean rendererOnly ) {
 	if ( !cls.soundStarted ) {
 		cls.soundStarted = qtrue;
 		S_Init();
-		//int test = WiseForward(2);
-		//int test2 = test;
 	}
 
 	if ( !cls.soundRegistered ) {
